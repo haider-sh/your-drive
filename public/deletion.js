4bd130e23eeb6c3ae223156ff4af132677539fcf
semProject-stupid <try.another.son@gmail.com>
2025-08-01 03:25:11 +0500

    Minor changes to frontend

--- public/deletion.js
+++ public/deletion.js
@@ -1,12 +1,23 @@
-const deleteButton = document.querySelector("#file-deletion");
-deleteButton.addEventListener("click", deleteFile);
+const deleteButton = document.querySelector("#file-deletion"); 
+const deleteButton2 = document.querySelector("#folder-deletion"); 
+
+if (deleteButton) {
+    console.log("delete button 1 is ", deleteButton);
+    deleteButton.addEventListener("click", deleteFile);
+}
+if (deleteButton2) {
+    console.log("delete button 2 is ", deleteButton2);
+    deleteButton2.addEventListener("click", deleteFile);
+}
 
 function deleteFile(event) {
-    const confirmation = confirm("Are you sure you want to delete this file?");
+    console.log("haider paglo");
+    const confirmation = confirm("Are you sure you want to proceed to deletion?");
     if (!confirmation) {
         event.preventDefault();
     }
     else {
-        alert("Your file has been deleted.")
+        alert("Deletion completed.")
     }
 }
+
